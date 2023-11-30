9f484014e2a65beeb35a7bb589bb0af80f137fa3
Maurice <abuguja.maurice@teranium.co>
2023-11-30 14:05:55 +0100

    ui fixes

--- src/components/dashboard/cards/SliderCard.jsx
+++ src/components/dashboard/cards/SliderCard.jsx
@@ -7,9 +7,11 @@ const SliderCard = ({ src, title, subTitle, btnText }) => {
     <div className={"sliderCardContainer"}>
       <img src={src} alt="sliderCardImage" />
       <div className={"sliderCardContent"}>
-        <Text>{title}</Text>
-        <Text>{subTitle}</Text>
-        <Button text={btnText} />
+        <Text tag={"h3"}>{title}</Text>
+        <Text className={"my-1 fs-14"} tag={"p"}>
+          {subTitle}
+        </Text>
+        <Button text={btnText} className="swiper-btn" />
       </div>
     </div>
   );
